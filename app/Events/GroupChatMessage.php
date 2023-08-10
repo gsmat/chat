a4fd82a1011cddf4d20a347112e3e360aecbc9ce
Gismat Guseinov <106599109+gsmat@users.noreply.github.com>
2023-08-10 09:48:32 +0400

    first commit

---
+++ app/Events/GroupChatMessage.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Events;
+
+use BeyondCode\LaravelWebSockets\WebSockets\Channels\PresenceChannel;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class GroupChatMessage implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $message;
+    /**
+     * Create a new event instance.
+     */
+    public function __construct($message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PresenceChannel('group_chat' . $this->message->group_id),
+        ];
+    }
+}
